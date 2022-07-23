cd7c347fbdfe9987c0bb586419e53abee1386d49
social-dev-crew <87090732+sinage3080@users.noreply.github.com>
2022-07-23 12:57:15 +0900

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://wesf-292026.ingress-erytho.ewp.live/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://wesf-292026.ingress-erytho.ewp.live/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://wesf-292026.ingress-erytho.ewp.live/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://wesf-292026.ingress-erytho.ewp.live/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://wesf-292026.ingress-erytho.ewp.live/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://wesf-292026.ingress-erytho.ewp.live/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
